28b79ffd18f8556fcaaf736a7effafbb938530c6
Charles Blaxland <charles.blaxland@gmail.com>
2016-01-15 11:19:54 +1100

    Add test for delete stack

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,31 @@ require 'bora'
 
 # Make sure we don't accidentally make calls to AWS during tests
 Aws.config[:stub_responses] = true
+
+def describe_stack_events_result(timestamp: Time.new, status: "CREATE_COMPLETE", reason: nil)
+  OpenStruct.new({
+    stack_events: [
+      OpenStruct.new({
+        timestamp: timestamp,
+        resource_type: "AWS::CloudFormation::Stack",
+        logical_resource_id: TEST_STACK_NAME,
+        resource_status: status,
+        resource_status_reason: reason
+      })
+    ]
+  })
+end
+
+def empty_describe_stack_events_result
+  OpenStruct.new({ stack_events: [] })
+end
+
+def describe_stacks_result(status: "CREATE_COMPLETE")
+  OpenStruct.new({
+    stacks: [
+      OpenStruct.new({
+        stack_status: status
+      })
+    ]
+  })
+end
--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -4,34 +4,6 @@ require 'spec_helper'
 
 TEST_STACK_NAME = "test-stack"
 
-def describe_stack_events_result(timestamp: Time.new, status: "CREATE_COMPLETE", reason: nil)
-  OpenStruct.new({
-    stack_events: [
-      OpenStruct.new({
-        timestamp: timestamp,
-        resource_type: "AWS::CloudFormation::Stack",
-        logical_resource_id: TEST_STACK_NAME,
-        resource_status: status,
-        resource_status_reason: reason
-      })
-    ]
-  })
-end
-
-def empty_describe_stack_events_result
-  OpenStruct.new({ stack_events: [] })
-end
-
-def describe_stacks_result(status: "CREATE_COMPLETE")
-  OpenStruct.new({
-    stacks: [
-      OpenStruct.new({
-        stack_status: status
-      })
-    ]
-  })
-end
-
 describe Bora::Stack do
   before :each do
     @cfn = double(Aws::CloudFormation::Client)
@@ -39,24 +11,20 @@ describe Bora::Stack do
   end
 
   context "when the stack does not yet exist" do
-
-    before :each do
+    def setup_create_stack
       @describe_stacks_call_count = 0
       allow(@cfn).to receive(:describe_stacks) do
         @describe_stacks_call_count += 1
         raise Aws::CloudFormation::Errors::ValidationError.new("Stack does not exist", "Error") if @describe_stacks_call_count == 1
         describe_stacks_result
       end
-      @stack = Bora::Stack.new(TEST_STACK_NAME)
+      allow(@cfn).to receive(:describe_stack_events).and_return(empty_describe_stack_events_result, describe_stack_events_result(reason: "just because"))
     end
 
-    describe "#create" do
-      it "creates the stack" do
-        allow(@cfn).to receive(:describe_stack_events).with(anything).and_return(empty_describe_stack_events_result, describe_stack_events_result(reason: "just because"))
-        options = { stack_name: TEST_STACK_NAME, template_body: "foo" }
-        expect(@cfn).to receive(:create_stack).with(options)
-        @stack.create(options) { |e| expect(e.resource_status_reason).to eq("just because") }
-      end
+    before :each do
+      allow(@cfn).to receive(:describe_stacks).and_raise(Aws::CloudFormation::Errors::ValidationError.new("Stack does not exist", "Error"))
+      allow(@cfn).to receive(:describe_stack_events).at_least(:once).and_return(empty_describe_stack_events_result)
+      @stack = Bora::Stack.new(TEST_STACK_NAME)
     end
 
     describe "#exists?" do
@@ -65,12 +33,37 @@ describe Bora::Stack do
       end
     end
 
+    describe "#create" do
+      it "creates the stack" do
+        setup_create_stack
+        options = { stack_name: TEST_STACK_NAME, template_body: "foo" }
+        expect(@cfn).to receive(:create_stack).with(options)
+        @stack.create(options) { |e| expect(e.resource_status_reason).to eq("just because") }
+      end
+    end
+
+    describe "#create_or_update" do
+      it "calls create" do
+        setup_create_stack
+        allow(@cfn).to receive(:describe_stack_events).and_return(empty_describe_stack_events_result, describe_stack_events_result(reason: "just because"))
+        expect(@cfn).to receive(:create_stack)
+        @stack.create_or_update({})
+      end
+    end
+
+    describe "#delete" do
+      it "doesn't fail" do
+        expect(@cfn).to_not receive(:delete_stack)
+        expect(@stack.delete).to be_truthy
+      end
+    end
+
   end
 
   describe "#update" do
     it "updates the stack" do
       allow(@cfn).to receive(:describe_stacks).and_return(describe_stacks_result)
-      allow(@cfn).to receive(:describe_stack_events).with(anything).and_return(describe_stack_events_result(timestamp: Time.now - 60), describe_stack_events_result(reason: "just because"))
+      allow(@cfn).to receive(:describe_stack_events).and_return(describe_stack_events_result(timestamp: Time.now - 60), describe_stack_events_result(reason: "just because"))
 
       options = { stack_name: TEST_STACK_NAME, template_body: "foo" }
       expect(@cfn).to receive(:update_stack).with(options)
