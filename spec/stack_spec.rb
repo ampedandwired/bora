cd880075e9b2beeaa8f647103f7331852d7d9b82
Charles Blaxland <charles.blaxland@gmail.com>
2016-01-15 11:03:44 +1100

    Add test for update stack

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -4,11 +4,11 @@ require 'spec_helper'
 
 TEST_STACK_NAME = "test-stack"
 
-def describe_stack_events_result(status: "CREATE_COMPLETE", reason: nil)
+def describe_stack_events_result(timestamp: Time.new, status: "CREATE_COMPLETE", reason: nil)
   OpenStruct.new({
     stack_events: [
       OpenStruct.new({
-        timestamp: Time.new,
+        timestamp: timestamp,
         resource_type: "AWS::CloudFormation::Stack",
         logical_resource_id: TEST_STACK_NAME,
         resource_status: status,
@@ -33,25 +33,38 @@ def describe_stacks_result(status: "CREATE_COMPLETE")
 end
 
 describe Bora::Stack do
+  before :each do
+    @cfn = double(Aws::CloudFormation::Client)
+    allow(Aws::CloudFormation::Client).to receive(:new).and_return(@cfn)
+  end
+
   describe "#create" do
-    context "when the stack does not currently exist" do
-      it "creates the stack" do
-        @cfn = double(Aws::CloudFormation::Client)
-        allow(Aws::CloudFormation::Client).to receive(:new).and_return(@cfn)
-        expect(@cfn).to receive(:create_stack).with(anything)
-
-        @call_count = 0
-        allow(@cfn).to receive(:describe_stacks) do
-          @call_count += 1
-          raise Aws::CloudFormation::Errors::ValidationError.new("Stack does not exist", "Error") if @call_count == 1
-          describe_stacks_result
-        end
-
-        expect(@cfn).to receive(:describe_stack_events).with(anything).and_return(empty_describe_stack_events_result, describe_stack_events_result(reason: "just because"))
-
-        @stack = Bora::Stack.new(TEST_STACK_NAME)
-        @stack.create({}) { |e| expect(e.resource_status_reason).to eq("just because") }
+    it "creates the stack" do
+      @call_count = 0
+      allow(@cfn).to receive(:describe_stacks) do
+        @call_count += 1
+        raise Aws::CloudFormation::Errors::ValidationError.new("Stack does not exist", "Error") if @call_count == 1
+        describe_stacks_result
       end
+
+      allow(@cfn).to receive(:describe_stack_events).with(anything).and_return(empty_describe_stack_events_result, describe_stack_events_result(reason: "just because"))
+
+      options = { stack_name: TEST_STACK_NAME, template_body: "foo" }
+      expect(@cfn).to receive(:create_stack).with(options)
+      @stack = Bora::Stack.new(TEST_STACK_NAME)
+      @stack.create(options) { |e| expect(e.resource_status_reason).to eq("just because") }
+    end
+  end
+
+  describe "#update" do
+    it "updates the stack" do
+      allow(@cfn).to receive(:describe_stacks).and_return(describe_stacks_result)
+      allow(@cfn).to receive(:describe_stack_events).with(anything).and_return(describe_stack_events_result(timestamp: Time.now - 60), describe_stack_events_result(reason: "just because"))
+
+      options = { stack_name: TEST_STACK_NAME, template_body: "foo" }
+      expect(@cfn).to receive(:update_stack).with(options)
+      @stack = Bora::Stack.new(TEST_STACK_NAME)
+      @stack.update(options) { |e| expect(e.resource_status_reason).to eq("just because") }
     end
   end
 end
