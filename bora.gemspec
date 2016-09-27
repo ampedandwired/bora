6cf6fc76d968e7417b541deb8ddb7d6464c71542
Charles Blaxland <charles.blaxland@gmail.com>
2016-09-27 21:33:16 +1000

    Add test code coverage report

--- bora.gemspec
+++ bora.gemspec
@@ -26,4 +26,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.11"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "simplecov", "~> 0.12"
 end
--- spec/helper/spec_helper.rb
+++ spec/helper/spec_helper.rb
@@ -1,4 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/.direnv/"
+  add_filter "/spec/"
+end
+
 require 'aws-sdk'
 require 'bora'
 require 'helper/cfn_helper'
